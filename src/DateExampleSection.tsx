0bff83c5b7d8bc1f27c956016d14b61bf41a586b
50ra4 <50ra4.dev.dev@gmail.com>
2025-03-16 15:25:50 +0900

    introduce: tailwindcss v4

--- src/DateExampleSection.tsx
+++ src/DateExampleSection.tsx
@@ -1,7 +1,7 @@
 export function DateExampleSection() {
   return (
-    <section>
-      <h2>Date String Example</h2>
+    <section className="m-4 p-5 border border-pink-600">
+      <h2 className="text-pink-600 text-3xl font-bold">Date String Example</h2>
       <div>Example ...</div>
     </section>
   );
